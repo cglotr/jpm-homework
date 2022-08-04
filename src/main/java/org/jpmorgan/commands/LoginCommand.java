a59489e8ce34749d58aba9e650b0934e423a1ec8
cglotr <amir.ariffin.1992@gmail.com>
2022-08-04 16:32:28 +0800

    test all cmds

--- src/main/java/org/jpmorgan/commands/LoginCommand.java
+++
@@ -1,8 +0,0 @@
-package org.jpmorgan.commands;
-
-public class LoginCommand implements Command {
-    @Override
-    public boolean isValid() {
-        return true;
-    }
-}
--- src/test/java/org/jpmorgan/utils/InputUtilTest.java
+++ src/test/java/org/jpmorgan/utils/InputUtilTest.java
@@ -3,13 +3,38 @@ package org.jpmorgan.utils;
 import org.jpmorgan.enums.Command;
 import org.junit.jupiter.api.Test;
 
-import static org.jpmorgan.enums.Command.SETUP;
+import java.util.Arrays;
+
+import static org.jpmorgan.enums.Command.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputUtilTest {
     @Test
     public void parseCommandType() {
-        Command actual = InputUtil.parseCommandType("Setup #SHOW1 10 10 5");
-        assertEquals(SETUP, actual);
+        TestCase[] tests = {
+                new TestCase("Setup", SETUP),
+                new TestCase("View", VIEW),
+                new TestCase("Availability", AVAILABILITY),
+                new TestCase("Book", BOOK),
+                new TestCase("Cancel", CANCEL),
+                new TestCase("Exit", EXIT),
+                new TestCase("Login", LOGIN),
+                new TestCase("Logout", LOGOUT),
+                new TestCase("", INVALID),
+                new TestCase("?", INVALID)
+        };
+        Arrays.stream(tests).forEach(testCase -> {
+            assertEquals(testCase.expected, InputUtil.parseCommandType(testCase.input));
+        });
+    }
+
+    private static class TestCase {
+        public String input;
+        public Command expected;
+
+        TestCase(String input, Command expected) {
+            this.input = input;
+            this.expected = expected;
+        }
     }
 }
